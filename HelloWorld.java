522c602e918a783e0524edb5e271438f4f4cb1c0
Hemanth B <hemanth22hemu@gmail.com>
2018-06-14 12:34:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld
 {
 	public static void main(String h[])
 		{
-		System.out.println("Hello World !");
+		System.out.println("Hello World to Java !");
+		System.out.println("This is sample java program testing in Travis CI with Java Officical Docker Repository");
+		System.out.println("Link to docker repository : https://hub.docker.com/_/java/");
 }
 }
